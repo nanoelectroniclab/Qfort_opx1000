ef33755f29eb504599cc40360fa9a838eff32f85
poyang <poyang1227@gmail.com>
2026-07-06 15:14:22 +0800

    Remove unused unit import and u variable (22_z_gate_error_amplification)

--- calibrations/22_z_gate_error_amplification.py
+++ calibrations/22_z_gate_error_amplification.py
@@ -9,7 +9,6 @@ from qm.qua import *
 from qualang_tools.loops import from_array
 from qualang_tools.multi_user import qm_session
 from qualang_tools.results import progress_counter
-from qualang_tools.units import unit
 
 from qualibrate import QualibrationNode
 from quam_config import Quam
@@ -65,7 +64,6 @@ node.machine = Quam.load()
 # %% {Create_QUA_program}
 @node.run_action(skip_if=node.parameters.load_data_id is not None)
 def create_qua_program(node: QualibrationNode[Parameters, Quam]):
-    u = unit(coerce_to_integer=True)
     node.namespace["qubits"] = qubits = get_qubits(node)
     num_qubits = len(qubits)
     operation = node.parameters.operation
